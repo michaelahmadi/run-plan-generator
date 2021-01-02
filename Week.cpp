7b973abd67fd8215ae6495db452602fca782e453
Michael Ahmadi <mahmad37@uic.edu>
2021-01-02 00:29:16 -0600

    Made the starting mileage dynamic, commented in planned week structure

--- Week.cpp
+++ Week.cpp
@@ -8,6 +8,16 @@ Week::Week()
 Week::Week(int mileage)
 {
     this->mileage = mileage;
+
+    // Generate a week that has the given mileage using the following format:
+    // Medium run 1
+    // Medium Run 2
+    // Easy Run 
+    // Tempo or Speed Intervals Run
+    // Easy Run
+    // Long Run that is .30 * mileage
+    // Recovery Day, 0 miles
+
 }
 
 int Week::getMileage()
--- runPlanGenerator.cpp
+++ runPlanGenerator.cpp
@@ -2,23 +2,25 @@
 
 int main(int argc, char *argv[])
 {
-    int goalMileage;
+    int startingMileage; // starting weekly mileage
+    int goalMileage; // weekly mileage wanting to be achieved
 
     // a bit of command line input handling
-    if (argc < 2) // goal mileage was not given in the command line
+    if(argc < 3) // goal mileage was not given in the command line
     {
-        std::cout << "Correct Usage: ./runPlanGenerator [goalMileage]";
+        std::cout << "Correct Usage: ./runPlanGenerator [startingMileage] [goalMileage]";
         return 0;
     }
-    goalMileage = *argv[1];
-    if(goalMileage < 10)
+    startingMileage = *argv[1];
+    goalMileage = *argv[2];
+    if(startingMileage < 10 || goalMileage < 10)
     {
-        std::cout << "Please input a weekly mileage that is at least 10";
+        std::cout << "Please input a starting mileage and goal mileage that are each at least 10 miles";
         return 0;
     }
     
     std::vector<Week> weeks; // Vector to hold each week generated
-    int currentMileage = 10; // Mileage of the week currently being generated
+    int currentMileage = startingMileage; // Mileage of the week currently being generated
     int stepCounter = 0; // Tracks the number of increasing weeks in a row
 
     do
